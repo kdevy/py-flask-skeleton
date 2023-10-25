2079715ebf994f2d080c4300c4cba63517d007fe
kdevy <ykinoshita.cal0@gmail.com>
2023-10-26 02:06:20 +0900

    display sitemap

--- app/modules/main/views/TopView.py
+++ app/modules/main/views/TopView.py
@@ -1,5 +1,5 @@
 from flask.views import View
-from flask import render_template
+from flask import render_template, current_app, url_for
 from flask_login import current_user
 
 class TopView(View):
@@ -7,5 +7,15 @@ class TopView(View):
         self.template = template
 
     def dispatch_request(self):
-        values = current_user
-        return render_template(self.template, data=values)
+        map = []
+        for rule in current_app.url_map.iter_rules():
+
+            options = {}
+            for arg in rule.arguments:
+                options[arg] = "[{0}]".format(arg)
+
+            methods = ','.join(rule.methods)
+            url = url_for(rule.endpoint, **options)
+            route = {"endpoint": rule.endpoint, "methods": methods, "url": url}
+            map.append(route)
+        return render_template(self.template, user=current_user, map=map)
