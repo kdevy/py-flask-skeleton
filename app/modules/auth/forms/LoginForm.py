39ef8ca6e2165e6c8275fae4865ec6be331ecc9d
kdevy <ykinoshita.cal0@gmail.com>
2023-10-26 01:06:32 +0900

    login form replaced flask_wtf

--- app/modules/auth/forms/LoginForm.py
+++ app/modules/auth/forms/LoginForm.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, EmailField
+from wtforms import PasswordField, EmailField, BooleanField
 from wtforms.validators import InputRequired, Email
 
 class LoginForm(FlaskForm):
@@ -8,9 +8,14 @@ class LoginForm(FlaskForm):
       validators=[
         InputRequired("メールアドレスを入力してください。"),
         Email("メールアドレス形式で入力してください。")
-      ]
+      ],
+      render_kw={"placeholder": "Email"}
     )
     password = PasswordField(
       "Password",
-      validators=[InputRequired("パスワードを入力してください。")]
+      validators=[
+        InputRequired("パスワードを入力してください。")
+      ],
+      render_kw={"placeholder": "Password"}
     )
+    remember = BooleanField('Remember me')
--- app/modules/auth/views/LoginView.py
+++ app/modules/auth/views/LoginView.py
@@ -3,10 +3,12 @@ from flask import render_template, request, jsonify
 from werkzeug.security import check_password_hash
 from flask_login import login_user
 from models.User import User
+from ..forms.LoginForm import LoginForm
 
 class LoginView(View):
     def __init__(self, template):
         self.template = template
+        self.form = LoginForm()
 
     def dispatch_request(self):
         if (request.method == "POST"):
@@ -15,24 +17,15 @@ class LoginView(View):
             if (action == "login"):
                 return self.login_post()
 
-        return render_template(self.template)
+        return render_template(self.template, form=self.form)
 
     def login_post(self):
-        email = request.form.get("email")
-        password = request.form.get("password")
-        remember = True if request.form.get('remember') else False
+        user = User.query.filter_by(email=self.form.email.data).first()
 
-        user = User.query.filter_by(email=email).first()
-
-        errors = []
-        if not email:
-            errors.append("メールアドレスを入力してください。")
-        if not password:
-            errors.append("パスワードを入力してください。")
-        if errors:
-            return jsonify({"ok":False, "errors": errors})
-        if not user or not check_password_hash(user.password, password):
+        if not self.form.validate_on_submit():
+            return jsonify({"ok":False, "errors": self.form.errors})
+        if not user or not check_password_hash(user.password, self.form.password.data):
             return jsonify({"ok":False, "errors": ["入力内容を確認してください。"]})
 
-        login_user(user, remember=remember)
+        login_user(user, remember=self.form.remember.data)
         return jsonify({"ok":True})
